3846438bccfaf7ddb69ff2729946ea019bee472b
kezheng.sun <kezheng.sun@xiaoying.com>
2025-02-06 10:29:21 +0800

    2025-02-06 10:29:21

--- src/main/java/com/sunkz/monitor/proxy/GithubClient.java
+++ src/main/java/com/sunkz/monitor/proxy/GithubClient.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "github-client", url = "https://api.github.com")
 public interface GithubClient {
 
+    /**
+     * <a href="https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28">...</a>
+     */
     @PutMapping(value = "/repos/${monitor.user}/${monitor.repo}/contents/{path}", headers = {"Accept=application/vnd.github+json", "X-GitHub-Api-Version=2022-11-28", "Authorization=Bearer ${monitor.token}"})
     void putContent(@PathVariable("path") String path, @RequestBody GithubRequest request);
 
